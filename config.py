88a3979eac5d014bdf1a792ce52415eac72e5e7f
Lucas Bacciotti <lbacciot@redhat.com>
2024-05-14 10:09:46 +0100

    feat: tests

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
+import os
+
+
 class DevConfig():
     MONGODB_SETTINGS = {
+        'db': os.getenv('MONGODB_DB'),
+        'host': os.getenv('MONGODB_HOST'),
+        'username': os.getenv('MONGODB_USER'),
+        'password': os.getenv('MONGODB_PASSWORD')
+    }
+
+
+class MockConfig():
+    MONGODB_SETTINGS = {
         'db': 'users',
-        'host': 'mongodb',
-        'username': "admin",
-        'password': "admin"
+        'host': 'mongomock://localhost',
     }
